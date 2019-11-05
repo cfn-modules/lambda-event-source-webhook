436c5e4c4883d056fcd16251b7d17db42b2a83eb
Michael Wittig <michael@widdix.de>
2019-11-05 10:06:54 +0100

    Update to Node 10

--- test/test.js
+++ test/test.js
@@ -1,13 +1,12 @@
 const test = require('ava');
 const cfntest = require('@cfn-modules/test');
-const axios = require('axios');
 
 test.serial('defaults', async t => {
   const stackName = cfntest.stackName();
   try {
     t.log(await cfntest.createStack(`${__dirname}/defaults.yml`, stackName, {}));
     const outputs = await cfntest.getStackOutputs(stackName);
-    const res = await axios.post(outputs.Url);
+    const res = await cfntest.probeHttpPost(outputs.Url);
     t.is(res.status, 204);
   } finally {
     t.log(await cfntest.deleteStack(stackName));
